02010d5f11e57f39caa45e6eb4a4119ff241697d
N1ck49871 <106702073+N1ck49871@users.noreply.github.com>
2022-10-18 10:04:07 -0300

    variavel categoria no cadastro

--- assets/php/usuarioController.php
+++ assets/php/usuarioController.php
@@ -5,8 +5,9 @@ include_once "conexao.php";
  $name = $_POST['nameUser'];
  $email = $_POST['email'];
  $pwd = $_POST['password'];
+ $cat = $_POST['categoria'];
 
- $dados[] = array($name, $email, $pwd); 
+ $dados[] = array($name, $email, $pwd,$cat); 
 
 
 class usuarioController{
@@ -22,10 +23,10 @@ class usuarioController{
 
     }
 
-     public function cadastrar($name ,$email, $pwd ){
+     public function cadastrar($name ,$email, $pwd,$cat ){
         $conn = new Conexao();
         $conn = $conn->conexao();
-        $stmt = $conn->prepare("INSERT INTO `usuario` (`idUsuario`, `nomeUsuario`, `emailUsuario`, `senhaUsuario`, `idCategoriaFK`) VALUES (NULL, ".$dados[0].",".$dados[1].",".$dados[2].");
+        $stmt = $conn->prepare("INSERT INTO `usuario` (`idUsuario`, `nomeUsuario`, `emailUsuario`, `senhaUsuario`, `idCategoriaFK`) VALUES (NULL, ".$dados[0].",".$dados[1].",".$dados[2].",".$dados[4].");
         $result = $stmt->execute();
         return $result;
      }
